365f6ca59ea7238d816b94c657f6ae2e9dae3c9a
Charith Dissanayake <charithdissanayake47@gmail.com>
2022-04-23 13:12:47 +0530

    added some template code

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
+    <h1 class="text-3xl font-bold">
+      Hello from App!
+    </h1>
     <nav>
       <router-link class="pr-2 text-sky-600 underline" to="/">Home</router-link>
       <router-link class="text-sky-600 underline" to="/about">About</router-link>
     </nav>
-    <h1 class="text-3xl font-bold">
-      Hello from App!
-    </h1>
-    <router-view>
-    </router-view>
+    <router-view></router-view>
   </div>
 </template>
 
-<script setup>
+<script>
+export default {
+  setup() {
+    const data = 'Hello from setup!'
+    return {
+      data
+    }
+  },
+  mounted() {
+    console.log(this.data)
+  }
+}
 </script>
-
-
-<style>
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,22 @@
         <h3>
             Hello from Home!
         </h3>
+        <span style="display: block;">{{ count }}</span>
+        <button @click="count++">Increment</button>
     </div>
 </template>
 
+<script>
+import { ref } from "vue"
+export default {
+    setup() {
+        const count = ref(0)
+        return {
+            count
+        }
+    },
+    mounted() {
+        console.log(this.count)
+    }
+}
+</script>
